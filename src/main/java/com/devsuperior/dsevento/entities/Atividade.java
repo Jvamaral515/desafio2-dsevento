3ebd41061c2b6f4a51552dc60e4ca0001aa60aba
Joao Victor <jv-amaralds@hotmail.com>
2024-02-28 09:01:48 -0300

    Relacionamento Categoria-Atividade

--- src/main/java/com/devsuperior/dsevento/entities/Atividade.java
+++ src/main/java/com/devsuperior/dsevento/entities/Atividade.java
@@ -13,6 +13,10 @@ public class Atividade {
     private String descricao;
     private Double preco;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
     public Atividade(){
 
     }
@@ -54,5 +58,13 @@ public class Atividade {
 
     public void setPreco(Double preco) {
         this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
 }
